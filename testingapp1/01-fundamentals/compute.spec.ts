57e1f07852ca27b033c50cd16922e6e12322133a
AlanRegan <alanregann@gmail.com>
2021-04-12 12:10:09 +0100

    initial commit

---
+++ testingapp1/01-fundamentals/compute.spec.ts
@@ -0,0 +1,12 @@
+import {compute} from '01-fundamentals/compute';
+
+describe('compute', () => {
+    it('should return 0 if the input is negative', () => {
+        const result = compute(-1);
+        expect(result).toBe(0);
+    });
+    it('should return 1 if the input is postive', () => {
+        const result = compute(1);
+        expect(result).toBe(2);
+    });
+});
---
+++ testingapp1/src/app/01-fundamentals/Arrays/getCurrencies.spec.ts
@@ -0,0 +1,7 @@
+import {getCurrencies} from './getCurrencies';
+
+// describe('getCurrencies', () => {
+//     it('it should include USD, GDP and EUR', () => {
+//         expect(getCurrencies('Frank')).toContain('Frank');
+//     });
+// });
